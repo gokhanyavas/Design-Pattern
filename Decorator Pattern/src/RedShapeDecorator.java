8e65d805ffe5424b6239438acbe6a0c2d759f10a
Gökhan Yavaş <yvsgkhn@gmail.com>
2017-04-30 14:19:27 +0300

    Decorator Pattern
    Decorator Pattern

---
+++ Decorator Pattern/src/RedShapeDecorator.java
@@ -0,0 +1,19 @@
+/**
+ * Created by gokhanyavas on 30.04.2017.
+ */
+public class RedShapeDecorator extends ShapeDecorator {
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape){
+        System.out.println("Border Color: Red");
+    }
+}
---
+++ Decorator Pattern/src/ShapeDecorator.java
@@ -0,0 +1,14 @@
+/**
+ * Created by gokhanyavas on 30.04.2017.
+ */
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape){
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw(){
+        decoratedShape.draw();
+    }
+}
