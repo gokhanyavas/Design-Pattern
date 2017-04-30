4c831d75fe9b23e64d77af19be93addd0fe9a5fa
Gökhan Yavaş <yvsgkhn@gmail.com>
2017-04-30 14:34:41 +0300

    Composite Pattern
    Composite Pattern

---
+++ Composite Pattern/src/Employee.java
@@ -0,0 +1,36 @@
+/**
+ * Created by gokhanyavas on 30.04.2017.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    // constructor
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+    }
+}
