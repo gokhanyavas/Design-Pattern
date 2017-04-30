fc5bfaf41ae0f949c71e4568b9c672b8820ff9f3
Gökhan Yavaş <yvsgkhn@gmail.com>
2017-04-30 14:40:16 +0300

    Proxy Pattern
    Proxy Pattern

---
+++ Proxy Pattern/src/ProxyImage.java
@@ -0,0 +1,20 @@
+/**
+ * Created by gokhanyavas on 30.04.2017.
+ */
+public class ProxyImage implements Image{
+
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if(realImage == null){
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
---
+++ Proxy Pattern/src/ProxyPatternDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by gokhanyavas on 30.04.2017.
+ */
+public class ProxyPatternDemo {
+
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        //image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        //image will not be loaded from disk
+        image.display();
+    }
+}
